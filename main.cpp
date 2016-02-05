03031dafc7fbe8fa07e099fe68cb26c764020ac8
tsteinholz <tsteinholz@yahoo.com>
2016-02-04 21:40:27 -0500

    fixed up main, on deck: assets

--- main.cpp
+++ main.cpp
@@ -10,52 +10,41 @@
 
 int main() {
 
-    const int SCREEN_W = 850, SCREEN_H = 650;
+    const unsigned int SCREEN_W = 850, SCREEN_H = 650;
 
     ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_EVENT_QUEUE *queue;
+    ALLEGRO_TIMER *timer;
 
-    ALLEGRO_EVENT_QUEUE* queue;
-
-    ALLEGRO_TIMER* timer;
-
-    bool render, executing = true;
-
-    if(!al_init())
-    {
+    if (!al_init()) {
         printf("al_init Failed!\n");
         return -1;
     }
-    if(!al_install_audio())
-    {
+    if (!al_install_audio()) {
         fprintf(stderr, "Failed to initialize audio!\n");
         return -1;
     }
 
-    if(!al_init_acodec_addon())
-    {
+    if (!al_init_acodec_addon()) {
         fprintf(stderr, "Failed to initialize audio codecs!\n");
         return -1;
     }
 
-    if (!al_reserve_samples(1))
-    {
+    if (!al_reserve_samples(1)) {
         fprintf(stderr, "Failed to reserve samples!\n");
         return -1;
     }
-    if(!al_install_mouse())
-    {
+    if (!al_install_mouse()) {
         fprintf(stderr, "Failed to initialize the mouse!\n");
         return -1;
     }
-    if(!al_init_primitives_addon())
-    {
+    if (!al_init_primitives_addon()) {
         printf("al_init_primitives_addon Failed!\n");
         return -1;
     }
     display = al_create_display(SCREEN_W, SCREEN_H);
 
-    if(!display)
-    {
+    if (!display) {
         printf("al_create_display Failed!\n");
         return -1;
     }
@@ -77,34 +66,24 @@ int main() {
 
     al_start_timer(timer);
 
-    while (executing)
-    {
+    bool render = true, executing = true;
+
+    while (executing) {
         ALLEGRO_EVENT event;
         al_wait_for_event(queue, &event);
-        switch(event.type)
-        {
-        case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-
-            break;
-        case ALLEGRO_EVENT_DISPLAY_CLOSE:
-            executing = false;
-            break;
-        case ALLEGRO_EVENT_KEY_DOWN:
-            switch(event.keyboard.keycode)
-            {
-            case ALLEGRO_KEY_ESCAPE:
+        switch (event.type) {
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
                 executing = false;
                 break;
-            case ALLEGRO_EVENT_KEY_UP:
+            case ALLEGRO_EVENT_KEY_DOWN:
+                executing = !event.keyboard.keycode == ALLEGRO_KEY_ESCAPE;
                 break;
             case ALLEGRO_EVENT_TIMER:
                 render = true;
                 break;
-            }
         }
-        if (al_is_event_queue_empty(queue) && render)
-        {
-            al_clear_to_color(al_map_rgb(0,0,0));
+        if (al_is_event_queue_empty(queue) && render) {
+            al_clear_to_color(al_map_rgb(0, 0, 0));
             al_set_target_bitmap(al_get_backbuffer(display));
             ////////////////////////////////////////////////////////////////////
 
